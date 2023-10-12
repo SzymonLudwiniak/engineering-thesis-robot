148c4ca0c3c24d2e1a29f0170e8e52aae8b148cf
Szymon Ludwiniak <259215@student.pwr.edu.pl>
2023-10-12 20:09:59 +0200

    main for stepper motor check

--- main/main.cpp
+++ main/main.cpp
@@ -4,27 +4,20 @@
 #include "externals/gpio/GPIO.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "hbridge/L293D_HBridge.h"
+#include "stepperMotor/ULN200X_StepperMotor.h"
 
 extern "C" void app_main() {
-    GPIO m1Enable(M1_ENABLE, PinMode::OUTPUT);
-    GPIO m2Enable(M2_ENABLE, PinMode::OUTPUT);
-    GPIO m1Controll1(M1_CONTROL_1, PinMode::OUTPUT);
-    GPIO m1Controll2(M1_CONTROL_2, PinMode::OUTPUT);
-    GPIO m2Controll1(M2_CONTROL_1, PinMode::OUTPUT);
-    GPIO m2Controll2(M2_CONTROL_2, PinMode::OUTPUT);
+    GPIO A(IN1, PinMode::OUTPUT);
+    GPIO B(IN2, PinMode::OUTPUT);
+    GPIO C(IN3, PinMode::OUTPUT);
+    GPIO D(IN4, PinMode::OUTPUT);
 
-    L293D_HBridge bridge(
-        m1Enable, m2Enable, m1Controll1, m1Controll2, m2Controll1, m2Controll2);
-
+    ULN200X_StepperMotor stepper(A, B, C, D);
 
     while (1) {
-        bridge.setMotorState(Motor::BOTH, 1);
-        bridge.start(Motor::BOTH, MotorDirection::FORWARD);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        bridge.setMotorState(Motor::MOTOR1, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        bridge.setMotorState(Motor::MOTOR2, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        stepper.rotateFor(180);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        stepper.rotateFor(-180);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
